f908b99e42e07914f18c1c6ec2001926d7796d55
Azul <azul@leap.se>
2012-09-01 13:52:11 +0200

    actually listen to the certs db and print the feed

--- test.rb
+++ test.rb
@@ -3,19 +3,16 @@
 require 'rubygems'
 require "yajl/http_stream"
 
-Signal.trap('INT') { AMQP.stop{ EM.stop } }
-Signal.trap('TERM'){ AMQP.stop{ EM.stop } }
-
 def main
   url = "http://localhost:5984/"
-  db = "test"
+  db = "leap_certs"
   feed_type = "continuous"
   since = "0"
   queue = "changes"
   
   Yajl::HttpStream.get("#{url}#{db}/_changes?feed=#{feed_type}&since=#{since}", :symbolize_keys => true) do |hash|
     if hash[:id]
-      #p hash
+      p hash
     end
   end
 end
