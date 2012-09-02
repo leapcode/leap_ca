ce01ff5ba54eda2748c52cac25004af551c7efc2
Azul <azul@leap.se>
2012-09-02 12:17:44 +0200

    use right db and only track new changes

--- test.rb
+++ test.rb
@@ -5,11 +5,16 @@ require "yajl/http_stream"
 
 def main
   url = "http://localhost:5984/"
-  db = "leap_certs"
+  db = "salticidae_certs"
   feed_type = "continuous"
   since = "0"
   queue = "changes"
-  
+
+ # descending = true for last changes first. limit = 1  
+  Yajl::HttpStream.get("#{url}#{db}/_changes?limit=1&descending=true", :symbolize_keys => true) do |hash|
+    since = hash[:last_seq]
+  end
+
   Yajl::HttpStream.get("#{url}#{db}/_changes?feed=#{feed_type}&since=#{since}", :symbolize_keys => true) do |hash|
     if hash[:id]
       p hash
