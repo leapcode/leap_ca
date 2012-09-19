e8353821b846e676b85736d748cb923d8854884e
Azul <azul@riseup.net>
2012-09-19 11:00:29 +0200

    deamonized script
    you can now call it with
    leap_ca.rb {start,restart,stop,run}
    
    run will run in foreground.

--- leap_ca.rb
+++ leap_ca.rb
@@ -1,19 +1,21 @@
 #!/usr/bin/ruby
 
 require 'rubygems'
+require 'daemons'
 require 'yajl/http_stream'
+
 require 'lib/cert'
 require 'lib/couch_stream'
 require 'lib/couch_changes'
 require 'lib/pool'
 
 
-def main
-  puts "Tracking #{Cert.database.root}"
-  couch = CouchStream.new(Cert.database.root)
-  changes = CouchChanges.new(couch)
-  pool = LeapCA::Pool.new(File.expand_path("../config/pool.yml", __FILE__))
-  pool.fill
+puts "Tracking #{Cert.database.root}"
+couch = CouchStream.new(Cert.database.root)
+changes = CouchChanges.new(couch)
+pool = LeapCA::Pool.new(File.expand_path("../config/pool.yml", __FILE__))
+pool.fill
+Daemons.run_proc('leap_ca.rb') do
   changes.follow do |hash|
     p hash
     if hash[:deleted]
@@ -21,5 +23,3 @@ def main
     end
   end
 end
-
-main
